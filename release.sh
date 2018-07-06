ba440f20e547a259deef90910a445da4f1fa3811
Fernando Ortiz <fortiz2k@gmail.com>
2018-07-06 18:24:37 -0500

    add release script automation.

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+. ./gu_tools.inc.sh
+
+cmd=$1
+if [ "x$cmd" = "x" ]; then
+  cmd=patch
+fi
+
+VER=$(curl -sL https://raw.githubusercontent.com/fsaintjacques/semver-tool/master/src/semver | bash -s -- bump $cmd $(cat VERSION))
+NEWVER=v${VER}
+read -r -p "Releasing $NEWVER... Press CTRL-c to cancel or Enter to continue.." -n 1 dummy
+if asksure; then
+  echo ${VER} > VERSION
+  sed -i.bak "s/VER=v.*/VER=${NEWVER}/" Makefile
+  rm *.bak
+  git status
+  git commit -a -m "bump to version ${VER}"
+  git tag -a ${NEWVER} -m "Version ${VER}"
+fi
